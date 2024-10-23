a6e430fcf5f921b889e0e2dba0c5f781e005b329
Berkay Polat <berkaypolat@Berkays-MacBook-Pro.local>
2024-10-23 12:50:42 -0400

    The upvote and comments functionalities are added.

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,41 +1,70 @@
 import express from 'express'
+import {connectToDb, db} from "./db.js"
 
 const app = express();
-
 app.use(express.json());
 
-let articlesInfo = [{
-    name: "learn-react",
-    upvotes: 0,
-}, {
-    name: "learn-node",
-    upvotes: 0
-}, {
-    name: "mongodb",
-    upvotes: 0
-}]
+
+connectToDb(() => {
+    console.log("Sucessfully connected to the mongoDB server.");
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000.");
+    })
+})
+
+app.get("/api/articles/:name", async (req, res) => {
+    const {name} = req.params;
+    const article = await db.collection('articles').findOne({ name });
+    if (article) {
+        res.json(article);
+    } else {
+        res.status(404).send("Article not found!");
+    }
+})
+
+app.put("/api/articles/:name/upvote", async (req, res) => {
+    const {name} = req.params;
+
+    const returnObject = await db.collection('articles').updateOne(
+        {name: name},
+        {$inc: {upvotes: 1}}
+    );
+
+    const article = await db.collection('articles').findOne({ name });
+    res.json(article.upvotes);
+})
+
+
+app.post("/api/articles/:name/comments", async (req, res) => {
+    try {
+        const { name } = req.params;
+        const { postedBy, text } = req.body;
+    
+        await db.collection("articles").updateOne(
+          { name: name },
+          { $push: { comments: { postedBy, text } } }
+        );
+    
+        const article = await db.collection("articles").findOne({ name: name });
+        res.send(article.comments);
+        
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+
+})
+
+// learning mongoDB.
+
+/* app.get("/hello/:name", (req, res) => {
+    const {name} = req.params;
+    res.send(`Hello, ${name}`);
+})
 
 app.post("/", (req, res) => {
     res.send("GET request received.");
     console.log(`Received. Welcome, ${req.body.name}`);
 })
 
-app.put("/api/articles/:name/upvote", (req, res) => {
-    const nameI = req.params.name;
-    const article = articlesInfo.find((a) => a.name === nameI);
-    if (article) {
-        article.upvotes += 1;
-        res.send(`The article ${nameI} now has ${article.upvotes} upvotes.`);
-    } else {
-        res.send(`The article ${nameI} does not exist!`);
-    }
-})
-
-app.get("/hello/:name", (req, res) => {
-    const {name} = req.params;
-    res.send(`Hello, ${name}`);
-})
-
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000.");
-})
+*/
